50553dacd5307284ec957d3c6ea8a784de309994
root@mssql <root@mssql.local>
2025-09-26 21:37:05 +0000

    admin:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: { extend: {} },
   plugins: [require("tailwindcss-animate")],
